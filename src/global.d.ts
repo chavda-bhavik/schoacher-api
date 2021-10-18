77c87aa2e85e57372e3012f4f7be8ed322f647a5
chavda-bhavik <bhavik.chvda@gmail.com>
2021-10-18 15:29:31 +0530

    removed sessions and made jwt-token to store in cookie

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,31 +1,34 @@
 import { Request, Response } from 'express';
 import { Employer, Teacher } from './entities';
 interface FileField {
-    path: string
-    uploadName: string
-    mimetype: string
+    path: string;
+    uploadName: string;
+    mimetype: string;
 }
 interface sessionStore {
     token: string;
     userType: 'teacher' | 'employer';
 }
+interface cookiesStore {
+    token?: string;
+}
 type MyContext = {
-    req: Request & { session: sessionStore };
+    req: Request & { cookies: cookiesStore };
     res: Response;
     user: Teacher | Employer;
 };
 type TeacherContext = {
-    req: Request & { session: sessionStore };
+    req: Request & { cookies: cookiesStore };
     res: Response;
     user: Teacher;
-}
+};
 type EmployerContext = {
-    req: Request & { session: sessionStore };
+    req: Request & { cookies: cookiesStore };
     res: Response;
     user: Employer;
-}
+};
 interface TokenDecoded {
     id: number;
     type: 'teacher' | 'employer';
-    iat?: number
+    iat?: number;
 }
