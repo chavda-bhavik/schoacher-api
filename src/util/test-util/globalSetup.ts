155a0b7c34f6b602c52825171d96958d3c30ed31
chavda-bhavik <bhavik.chvda@gmail.com>
2021-10-23 10:49:52 +0530

    added tests for qualifications

--- src/util/test-util/globalSetup.ts
+++ src/util/test-util/globalSetup.ts
@@ -1,5 +1,5 @@
 import { ConnectionOptions, getConnectionOptions, createConnection } from 'typeorm';
-import { Teacher, Employer } from '../../entities';
+import { Teacher, Employer, Qualification } from '../../entities';
 
 let teacherData: Partial<Teacher>[] = [
     {
@@ -16,6 +16,26 @@ let employerData: Partial<Employer>[] = [
         password: '$2a$12$DqTMH3E8j4YOSobwy1boY.7.ZsmUml1Ha.aACNs7BvVNdX4X7H4Ue',
     },
 ];
+let qualificationsData: Partial<Qualification>[] = [
+    {
+        start: '2021-09-18T05:56:23.926Z',
+        end: '2021-07-22T22:33:00.631Z',
+        college: 'Veum, Smitham and Gerhold',
+        degree: 'quisquam maxime quo',
+        grade: 'A+',
+        description: 'International',
+        teacherId: 1,
+    },
+    {
+        start: '2015-09-12',
+        end: '2016-07-22',
+        college: 'ABCD College',
+        degree: 'quisquam maxime quo',
+        grade: '87.45',
+        description: 'International asdfasdff asdf',
+        teacherId: 1,
+    },
+];
 
 export default async () => {
     const options: ConnectionOptions = await getConnectionOptions('test');
@@ -28,4 +48,5 @@ export default async () => {
     let qb = await conn.createQueryBuilder();
     await qb.insert().into(Teacher).values(teacherData).execute();
     await qb.insert().into(Employer).values(employerData).execute();
+    await qb.insert().into(Qualification).values(qualificationsData).execute();
 };
